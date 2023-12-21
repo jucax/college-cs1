3239578864f21531c7c83721d13abe1fdcb23811
jucax <jcarlosmartinez745@gmail.com>
2023-12-20 19:40:09 -0600

    Added lecture question 1.1.1

---
+++ lecture-questions/1-basic-java/topic-1.1/solution1_1_1.java
@@ -0,0 +1,10 @@
+public class solution1_1_1 {
+    public static void main (String[] args) {
+        System.out.println("T-minus-5");
+        System.out.println("T-minus-4");
+        System.out.println("T-minus-3");
+        System.out.println("T-minus-2");
+        System.out.println("T-minus-1");
+        System.out.println("Blast-off!");
+    }
+}
