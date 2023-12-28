7fed385f383cc15865306d35221ad5a0c3348181
jucax <jcarlosmartinez745@gmail.com>
2023-12-28 17:33:09 -0600

    Finished 3-object-oriented-programming

--- lecture-questions/3-object-oriented-programming/topic-3.4/ArrayUtils3_4_8.java
+++ lecture-questions/3-object-oriented-programming/topic-3.4/ArrayUtils3_4_8.java
@@ -1,6 +1,7 @@
 public class ArrayUtils3_4_8 {
     public void arrayAbsoluteValue(double[] array){
         for (int i = 0 ; i < array.length ; i ++){
+            
             if (array[i] < 0){
                 array[i] *= -1;    
             }
